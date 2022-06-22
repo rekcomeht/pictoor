68623d001ffec23be01d884a82019dec603a7ed1
Rekcomeht <justin.w.waller@gmail.com>
2022-06-21 22:56:41 -0700

    Update boot.py

--- RaspberryPi_JetsonNano/python/examples/boot.py
+++ RaspberryPi_JetsonNano/python/examples/boot.py
@@ -16,6 +16,22 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
+def boot():
+    epd = epd7in5_V2.EPD()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect (("8.8.8.8",80))
+    ipadd = s.getsockname()[0]
+    epd.init()
+    epd.Clear()
+
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+    Limage = Image.open(os.path.join(picdir, 'pupper.bmp'))
+    draw = ImageDraw.Draw(Limage)
+    draw.text((20, 25), 'my ip address is', font = font18, fill = 0)
+    draw.text((20, 50), str(ipadd), font = font18, fill = 0)
+    epd.display(epd.getbuffer(Limage))
+    epd.sleep()
 try:
     logging.info("epd7in5_V2 Demo")
     epd = epd7in5_V2.EPD()
