26832796afe9214b788b848675e6549152eb824b
Rekcomeht <justin.w.waller@gmail.com>
2022-06-21 22:59:28 -0700

    Update blerp.py

--- RaspberryPi_JetsonNano/python/examples/blerp.py
+++ RaspberryPi_JetsonNano/python/examples/blerp.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 from bottle import route, run, template
-import test
+import test, boot
 
 @route('/change/<name>')
 def index(name):
     return test.picdisplay(name)
+
+@route('/boot/')
+def index():
+    return boot.boot()
   
 run(host='0.0.0.0', port=8080)
