c3af27b9f966e19af8d72fd343305e5ba8f86ddf
Rekcomeht <justin.w.waller@gmail.com>
2022-06-22 22:37:11 -0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,8 @@
+
+
+git pull
+
+sudo cp ~/pictoor/wpa_supplicant.conf /etc/wpa_supplicant/
+sudo chmod u+x ~/pictoor/RaspberryPi_JetsonNano/python/examples/boot.py
+sudo chmod u+x ~/pictoor/RaspberryPi_JetsonNano/python/examples/test.py
+sudo chmod u+x ~/pictoor/RaspberryPi_JetsonNano/python/examples/blerp.py
