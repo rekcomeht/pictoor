3f9ac6d77884fc4507f9136313d87451d4434046
Rekcomeht <justin.w.waller@gmail.com>
2022-06-24 23:52:39 -0700

    Update easyconvert.sh

--- convert/easyconvert.sh
+++ convert/easyconvert.sh
@@ -5,5 +5,5 @@ read source
 echo 'type the extension of the file you want to convert (e.g. the png part of example.png)'
 read ext
 
-convert $source+'.'+$ext -dither FloydSteinberg -define dither:diffusion-amount=85% -remap eink-3color.png BMP3:~pictoor/RaspberryPi_JetsonNano/python/pic/$source.bmp
+convert $source.$ext -dither FloydSteinberg -define dither:diffusion-amount=85% -remap eink-3color.png BMP3:~pictoor/RaspberryPi_JetsonNano/python/pic/$source.bmp
 
