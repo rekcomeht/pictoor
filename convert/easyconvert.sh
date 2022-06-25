b5331a72a235d740353ff05c377a385fed6d49ee
Rekcomeht <justin.w.waller@gmail.com>
2022-06-24 23:51:39 -0700

    Update easyconvert.sh

--- convert/easyconvert.sh
+++ convert/easyconvert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # easily convert a file into epaper format
-echo type the name of the file you want to convert (e.g. the example part of example.png)
+echo 'type the name of the file you want to convert (e.g. the example part of example.png)'
 read source
-echo type the extension of the file you want to convert (e.g. the png part of example.png)
+echo 'type the extension of the file you want to convert (e.g. the png part of example.png)'
 read ext
 
 convert $source+'.'+$ext -dither FloydSteinberg -define dither:diffusion-amount=85% -remap eink-3color.png BMP3:~pictoor/RaspberryPi_JetsonNano/python/pic/$source.bmp
